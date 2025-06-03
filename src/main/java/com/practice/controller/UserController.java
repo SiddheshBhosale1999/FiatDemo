c556fda3d1c61a4b7f4004968853f45c5c5e1e5a
Siddhesh Bhosale <siddesh.bhosale@ats-group.com>
2025-06-03 11:00:02 +0530

    Second commit

--- src/main/java/com/practice/controller/UserController.java
+++ src/main/java/com/practice/controller/UserController.java
@@ -1,8 +1,14 @@
 package com.practice.controller;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,63 +28,76 @@ public class UserController {
 
 	@Autowired
 	private UserRepository userRepository;
-	
-	
-	//method for adding common data to response
+
+	// method for adding common data to response
 	@ModelAttribute
-	public String addCommonData(Model model,Principal principal) {
+	public String addCommonData(Model model, Principal principal) {
 		String userName = principal.getName();
-		System.out.println("userName"+userName);
-		
-		//get the user using username(email)
+		System.out.println("userName" + userName);
+
+		// get the user using username(email)
 		User user = userRepository.getUserByUserName(userName);
-		
-		System.out.println("USER"+ user);
-		
+
+		System.out.println("USER" + user);
+
 		model.addAttribute("user", user);
 		return null;
-		
+
 	}
-	
-	//dashboard home
+
+	// dashboard home
 	@GetMapping("/index")
-	public String dashboard(Model model,Principal principal) {
+	public String dashboard(Model model, Principal principal) {
 
 		model.addAttribute("title", "User Dashboard");
 		return "normal/user_dashboard";
 	}
-	
-	//open add form handler
+
+	// open add form handler
 	@GetMapping("/add-contact")
-	public String openAddContact(Model model)
-	{
+	public String openAddContact(Model model) {
 		model.addAttribute("title", "Add Contact");
 		model.addAttribute("contact", new Contact());
-		
+
 		return "normal/add_contact_form";
 	}
-	
-	//processing add contact form
+
+	// processing add contact form
 	@PostMapping("/process-contact")
-	public String processContact(@ModelAttribute Contact contact,
-			@RequestParam("profileImage") MultipartFile file , 
+	public String processContact(@ModelAttribute Contact contact, @RequestParam("profileImage") MultipartFile file,
 			Principal principal) {
-	
+
 		try {
-		String name = principal.getName();
-		User user = this.userRepository.getUserByUserName(name);
-		
-		contact.setUser(user);
-		
-		user.getContacts().add(contact);
-		
-		this.userRepository.save(user);	
-		
-		System.out.println("DATA" + contact);
-		
-		System.out.println("Added to database");
-		}
-		catch (Exception e) {
+			String name = principal.getName();
+			User user = this.userRepository.getUserByUserName(name);
+
+			// processing and uploading file
+			if (file.isEmpty()) {
+				// if the file is empty then try our message
+				System.out.println("File is empty.");
+			} 
+			else {
+				// upload the file to folder and update the name to contact
+				contact.setImage(file.getOriginalFilename());
+
+				File saveFile = new ClassPathResource("static/img").getFile();
+
+				Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + file.getOriginalFilename());
+
+				Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+				System.out.println("Image is uploaded");
+			}
+
+			contact.setUser(user);
+
+			user.getContacts().add(contact);
+
+			this.userRepository.save(user);
+
+			System.out.println("DATA" + contact);
+
+			System.out.println("Added to database");
+		} catch (Exception e) {
 			System.out.println("ERROR" + e.getMessage());
 			e.printStackTrace();
 		}
